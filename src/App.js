94b370c9896fab171ac9fc962bcaa3d90aee1239
Javiuty <javiiescuadra@gmail.com>
2020-10-29 14:40:41 +0100

    adding form + header + styles + callApi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Formulario from "./components/Formulario";
+import Header from "./components/Header";
+import { API_KEY } from "./components/Key";
 
-function App() {
-  return <h1>Noticias</h1>;
-}
+const App = () => {
+  // definir la categoria y noticias
+  const [categoria, guardarCategoria] = useState("");
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `http://newsapi.org/v2/top-headlines?country=gb&category=${categoria}&apiKey=${API_KEY}`;
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      console.log(noticias);
+    };
+    consultarAPI();
+  }, [categoria]);
+
+  return (
+    <>
+      <Header titulo="Buscador De Noticias" />
+
+      <div className="container-white">
+        <Formulario guardarCategoria={guardarCategoria} />
+      </div>
+    </>
+  );
+};
 
 export default App;
